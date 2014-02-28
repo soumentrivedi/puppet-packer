e582dd7fc62c91f6f9d1b95556fd5070b656ae08
Nathan R Valentine <nrvale0@gmail.com>
2014-02-28 11:17:04 -0800

    prep for v0.1.3 bugfix

---
+++ pkg/nrvale0-packer-0.1.3/manifests/init.pp
@@ -0,0 +1,31 @@
+class packer(
+ $install_dir = $packer::params::install_dir,
+ $base_url = $packer::params::base_url,
+ $architecture = $::architecture,
+ $kernel = $::kernel,
+ $timeout = 600,
+ $version,
+) inherits packer::params {
+  validate_absolute_path($install_dir)
+  validate_string($base_url)
+  validate_re($architecture, ['^amd64$', '^arm$', '^i386$' ])
+  validate_re($kernel, ['^Linux$','^FreeBSD$','^OpenBSD$','^Windows$','^Darwin$'])
+  validate_string($version)
+
+  $package_name = downcase("${version}_${kernel}_${architecture}.zip")
+  $full_url = "${base_url}/${package_name}"
+
+  if !defined(Class['staging']) {
+    class { 'staging':
+      path => '/var/staging',
+      owner => 'puppet',
+      group => 'puppet',
+    }
+  }
+  
+  staging::file { $package_name: source => $full_url, } ->
+  staging::extract { $package_name:
+    target => $install_dir,
+    creates => "${install_dir}/packer",
+  }
+}
